519b3481c9feef1d18efb7c790a2178176e8f207
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2023-03-27 10:34:05 +0700

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9c2a7ea56516780325f6b81d4f4dd28f",
-"index.html": "5921c440024086ed7ff1cf0f421735b3",
-"/": "5921c440024086ed7ff1cf0f421735b3",
-"main.dart.js": "18e88fc29c0e5f333d23d1df0040b2fd",
+"index.html": "19b55ea7b4dd1ee2bc2515beb89a7f89",
+"/": "19b55ea7b4dd1ee2bc2515beb89a7f89",
+"main.dart.js": "0f92667825da4b91b1e67cfaced69ae0",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
